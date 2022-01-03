82ba7cf734174c1bb1623dc71f7d199e7e7d91ee
Tello- <tellovisionstudio@gmail.com>
2022-01-03 12:58:49 -0800

    updated ignore to include the needed files

---
+++ card/CardRequest.cs
@@ -0,0 +1,362 @@
+﻿namespace card
+{
+    [System.Serializable]
+    public class CardRequest
+    {
+        public CardData data { get; set; }
+    }
+
+    [System.Serializable]
+    public class MultiCardRequest
+    {
+        public CardData[] data { get; set; }
+        public int page { get; set; }
+        public int pageSize { get; set; }
+        public int count { get; set; }
+        public int totalCount { get; set; }
+
+        public override string ToString()
+        {
+            var retStr =
+                data.ToString() +
+                "\n" +
+                page.ToString() +
+                "\n" +
+                pageSize.ToString() +
+                "\n" +
+                count.ToString() +
+                "\n" +
+                totalCount.ToString();
+            return retStr;
+        }
+    }
+
+    [System.Serializable]
+    public class CardData
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string supertype { get; set; }
+        public string[] subtypes { get; set; }
+        public string   level { get; set; }
+        public string hp { get; set; }
+        public string[] types { get; set; }
+        public string evolvesFrom { get; set; }
+        public string[] evolvesTo { get; set; }
+        
+        public string[] rules { get; set; }
+        public AncientTrait ancientTrait { get; set; }
+        public Abilities[] abilities { get; set; }
+        public Attack[] attacks { get; set; }
+        public Weakness[] weaknesses { get; set; }
+        public Resistance[] resistances { get; set; }
+        public string[] retreatCost { get; set; }
+        public int convertedRetreatCost { get; set; }
+        public set.SetData set { get; set; }
+        public string number { get; set; }
+        public string artist { get; set; }
+        public string rarity { get; set; }
+        public int[] nationalPokedexNumbers { get; set; }
+        public set.Legalities legalities { get; set; }
+        public CardImages images { get; set; }
+        public Tcgplayer tcgplayer { get; set; }
+        public Cardmarket cardmarket { get; set; }
+
+        public override string ToString()
+        {
+            string retStr =
+                "ID: " + id +
+                "\nName: " + name +
+                "\nSuperType: " + supertype +
+                "\nSubtypes: ";
+
+            foreach (var subt in subtypes)
+            {
+                retStr += "\n\t" + subt;
+            }
+            
+            retStr += "\nLevel: " +level + "\n" +
+                "HP: " + hp + "\nTypes: ";
+
+            if (types != null)
+            {
+
+                foreach (var typ in types)
+                {
+                    retStr += "\t" + typ + "\n";
+                }
+            }
+
+            retStr += "\nEvolvesFrom: " + evolvesFrom + "\nEvolvesTo: ";
+
+            if (evolvesTo != null)
+            {
+                foreach (var evTo in evolvesTo)
+                {
+                    retStr += "\t" + evTo + "\n";
+                }
+            }
+
+            retStr += "\nRules: \n";
+            if (rules != null)
+            {
+                foreach (var rule in rules)
+                {
+                    retStr += ":: " + rule + "\n";
+                }
+            }
+
+            if (ancientTrait != null)
+            {
+                retStr +="\n" +
+                         ancientTrait.ToString();
+            }
+
+            if (abilities != null)
+            {
+                retStr += "\nAbilities: ";
+                foreach (var abl in abilities)
+                {
+                    retStr += "\n" +
+                        abl.ToString();
+                }
+                
+            }
+
+            if (attacks != null)
+            {
+                retStr += "\n\nAttacks:  ";
+                foreach (var attack in attacks)
+                {
+                    retStr += "\t" + attack.ToString();
+                }
+
+            }
+            
+            if (weaknesses != null)
+            {
+                retStr += "\nWeaknesses:  ";
+                foreach (var weak in weaknesses)
+                {
+                    retStr += weak.ToString();
+                }
+
+            }
+            
+            if (resistances != null)
+            {
+                retStr += "\n\nResistances:  ";
+                foreach (var resist in resistances)
+                {
+                    retStr += resist.ToString();
+                }
+
+            }
+            
+            retStr += "\n\nRetreat Cost: ";
+            if (retreatCost != null)
+            {
+                foreach (var retc in retreatCost)
+                {
+                    retStr += "\n\t" + retc;
+                }
+            }
+
+            retStr += "\nConverted Retreat Cost: " +
+                      convertedRetreatCost;
+            
+
+            return retStr;
+        }
+    }
+
+    [System.Serializable]
+    public class Resistance
+    {
+        public string type;
+        public string value;
+
+        public override string ToString()
+        {
+            return "\nType: " + type +
+                   "\nValue: " + value;
+        }
+    }
+
+    [System.Serializable]
+    public class Abilities
+    {
+        public string name { get; set; }
+        public string text { get; set; }
+        public string type { get; set; }
+
+        public override string ToString()
+        {
+            return
+                "\tType:\t" + type +
+                "\n\tName:\t" + name +
+                "\n" + text;
+        }
+    }
+
+    [System.Serializable]
+    public class AncientTrait
+    {
+        public string name { get; set; }
+        public string text { get; set; }
+
+        public override string ToString()
+        {
+           return "\nName: " + name + 
+                  "\nText: " + text;
+        }
+    }
+
+
+    [System.Serializable]
+    public class CardImages
+    {
+        public string small { get; set; }
+        public string large { get; set; }
+        
+        public override string ToString()
+        {
+            return "\nSmall: " + small + 
+                   "\nLarge: " + large;
+        }
+    }
+
+    [System.Serializable]
+    public class Tcgplayer
+    {
+        public string url { get; set; }
+        public string updatedAt { get; set; }
+        public Prices prices { get; set; }
+        public override string ToString()
+        {
+            return "\nUrl: " + url + 
+                   "\nUpdated At: " + updatedAt + 
+                   "\n" + prices.ToString();
+        }
+    }
+
+    [System.Serializable]
+    public class Prices
+    {
+        public Holofoil holofoil { get; set; }
+
+        public override string ToString()
+        {
+            return holofoil.ToString();
+        }
+    }
+
+    [System.Serializable]
+    public class Holofoil
+    {
+        public float low { get; set; }
+        public float mid { get; set; }
+        public float high { get; set; }
+        public float market { get; set; }
+
+        public override string ToString()
+        {
+            return "\nLow: " + low +
+                   "\nMid: " + mid +
+                   "\nHi: " + high +
+                   "\nMkt: " + market;
+        }
+    }
+
+    [System.Serializable]
+    public class Cardmarket
+    {
+        public string url { get; set; }
+        public string updatedAt { get; set; }
+        public Prices1 prices { get; set; }
+
+        public override string ToString()
+        {
+            return "\nUrl: " + url + 
+                   "\nUpdated At: " + updatedAt + 
+                   "\nPrices: " + prices.ToString();
+        }
+    }
+
+    [System.Serializable]
+    public class Prices1
+    {
+        public float averageSellPrice { get; set; }
+        public float lowPrice { get; set; }
+        public float trendPrice { get; set; }
+        public float reverseHoloTrend { get; set; }
+        public float lowPriceExPlus { get; set; }
+        public float avg1 { get; set; }
+        public float avg7 { get; set; }
+        public float avg30 { get; set; }
+        public float reverseHoloAvg1 { get; set; }
+        public float reverseHoloAvg7 { get; set; }
+        public float reverseHoloAvg30 { get; set; }
+
+        public override string ToString()
+        {
+            return "\naverageSellPrice: " + averageSellPrice + "\n" +
+                   "lowPrice: " + lowPrice + "\n" +
+                   "trendPrice: " + trendPrice + "\n" +
+                   "reverseHoloTrend: " + reverseHoloTrend + "\n" +
+                   "lowPriceExPlus: " + lowPriceExPlus + "\n" +
+                   "avg1: " + avg1 + "\n" +
+                   "avg7: " + avg7 + "\n" +
+                   "avg30: " + avg30 + "\n" +
+                   "reverseHoloAvg1: " + reverseHoloAvg1 + "\n" +
+                   "reverseHoloAvg7: " + reverseHoloAvg7 + "\n" +
+                   "reverseHoloAvg30: " + reverseHoloAvg30 + "\n";
+        }
+    }
+
+    [System.Serializable]
+    public class Attack
+    {
+        public string name { get; set; }
+        public string[] cost { get; set; }
+        public int convertedEnergyCost { get; set; }
+        public string damage { get; set; }
+        public string text { get; set; }
+
+        public override string ToString()
+        {
+            string retStr = "\nName: " + name + 
+                            "\nCost: \n";
+
+            foreach (var VARIABLE in cost)
+            {
+                retStr += "\t" + VARIABLE + "\n";
+            }
+
+            retStr += "\nConverted Energy Cost: " + convertedEnergyCost +
+                      "\nDamage: " + damage + 
+                      "\nText: \n\t" + text + "\n";
+
+            return retStr;
+
+
+        }
+    }
+
+    [System.Serializable]
+    public class Weakness
+    {
+        public string type { get; set; }
+        public string value { get; set; }
+
+        public override string ToString()
+        {
+            return
+                "\nType: " + type +
+                "\nValue: " + value;
+        }
+    }
+}
+
+
+
---
+++ set/SetRequest.cs
@@ -0,0 +1,95 @@
+﻿
+using System;
+
+namespace set
+{
+
+    
+    
+    public class SetRequest
+    {
+        public SetData data { get; set; }
+
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+    }
+
+    public class MultiSetRequest
+    {
+        public SetData[] data { get; set; }
+        public int page { get; set; }
+        public int pageSize { get; set; }
+        public int count { get; set; }
+        public int totalCount { get; set; }
+
+        public override string ToString()
+        {
+            return "\n" + data.ToString() +
+                   "\npage: " + page +
+                   "\npageSize: " + pageSize +
+                   "\ncount: " + count +
+                   "\ntotalCount: " + totalCount;
+        }
+    }
+
+    public class SetData : IDisposable
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string series { get; set; }
+        public int printedTotal { get; set; }
+        public int total { get; set; }
+        public Legalities legalities { get; set; }
+        public string ptcgoCode { get; set; }
+        public string releaseDate { get; set; }
+        public string updatedAt { get; set; }
+        public Images images { get; set; }
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "\nId: " + id +
+                   "\nName: " + name +
+                   "\nSeries: " + series +
+                   "\nPrinted Total: " + printedTotal +
+                   "\nTotal: " + total +
+                   "\nLegalities: " + legalities.ToString() +
+                   "\nptcGoCode: " + ptcgoCode +
+                   "\nRelease Date: " + releaseDate +
+                   "\nUpdated At: " + updatedAt +
+                   "\nImages: " + images.ToString();
+        }
+    }
+
+    public class Legalities
+    {
+        public string unlimited { get; set; }
+        public string standard { get; set; }
+        public string expanded { get; set; }
+
+        public override string ToString()
+        {
+            return "\nUnlimited: " + unlimited +
+                   "\nStandard: " + standard +
+                   "\nExpanded: " + expanded;
+        }
+    }
+
+    public class Images
+    {
+        public string symbol { get; set; }
+        public string logo { get; set; }
+
+        public override string ToString()
+        {
+            return "\nSymbol: " + symbol +
+                   "\nLogo: " + logo;
+        }
+    }
+
+}
